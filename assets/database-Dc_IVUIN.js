0782fef6f4cc39617c50b66a4d3bf8fdcce1b5da
vinmedrado <medrado.jobs@gmail.com>
2026-08-03 15:23:04 -0300

    Refine site and report visuals

---
+++ assets/database-Dc_IVUIN.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-D8bgLaiA.js";var t=e(`circle-check`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`path`,{d:`m9 12 2 2 4-4`,key:`dzmm74`}]]),n=e(`database`,[[`ellipse`,{cx:`12`,cy:`5`,rx:`9`,ry:`3`,key:`msslwz`}],[`path`,{d:`M3 5V19A9 3 0 0 0 21 19V5`,key:`1wlel7`}],[`path`,{d:`M3 12A9 3 0 0 0 21 12`,key:`mv7ke4`}]]);export{t as n,n as t};
